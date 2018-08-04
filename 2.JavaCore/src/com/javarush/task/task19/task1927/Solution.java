5b3465ea215549cd455e956e63b4cfd16271b87b
Alexandr <aevshvetsov@gmail.com>
2018-08-05 02:14:03 +0300

    Переворот строк

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1927/Solution.java
@@ -0,0 +1,41 @@
+package com.javarush.task.task19.task1927;
+
+/* 
+Контекстная реклама
+*/
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        PrintStream console = System.out;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream stream = new PrintStream(outputStream);
+        System.setOut(stream);
+        testString.printSomething();
+        String result = outputStream.toString();
+        System.setOut(console);
+        int count = 0;
+        for(String lines:result.split("\n")){
+            System.out.println(lines);
+            count++;
+            if(count%2==0){
+                System.out.println("JavaRush - курсы Java онлайн");
+            }
+
+        }
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("first");
+            System.out.println("second");
+            System.out.println("third");
+            System.out.println("fourth");
+            System.out.println("fifth");
+        }
+    }
+}
